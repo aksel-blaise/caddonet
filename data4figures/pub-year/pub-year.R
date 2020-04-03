92b187065906965a72e0ee210b5ab66246915a15
aksel-blaise <zac_selden@pm.me>
2020-04-03 06:02:10 -0500

    <edit ms>

--- data4figures/pub-year/pub-year.R
+++ data4figures/pub-year/pub-year.R
@@ -6,6 +6,5 @@ setwd(getwd())
 data<-read.csv(file = 'pub-year.csv')
 p<-ggplot(data = data, aes(x=year, y=num, color=year)) +
   geom_bar(stat="identity",fill="white") +
-  geom_vline(aes(xintercept=mean(year, na.rm=T)), color="red", linetype="dashed", size=1) +
-  geom_density(alpha=.3)
+  xlim(2000,2020)
 p
